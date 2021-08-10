6c5ed834e2e792ccf9461fe70f2a2b16ea562a20
jropero97 <jroperoco@uninpahu.edu.co>
2021-08-09 21:12:33 -0500

    python

---
+++ conditionals.py
@@ -0,0 +1,27 @@
+# x = 2
+# if x<30:
+#     print("x is lower than 30")
+# elif x==30:
+#     print("x is equal to 30")
+    
+# else:
+#     print("x is greater than 30")
+
+# name="Pepe"
+# last_name="Mujica"
+# if name=="Johan":
+#     if last_name=="Ropero":
+#         print("Welcome Johan")
+#     else:
+#         print("¿Quién putas es usted? Vayase a la verga. ¡¡¡Llamaré a la policía!!!")
+# else:
+#     print("¿Quién putas es usted? Vayase a la verga. ¡¡¡Llamaré a la policía!!!")
+X=input("Por favor califique de 1 a 10 que tanto le gustó el servicio")
+if int(X)<=5:
+    print("Gracias por su calificación, intentaremos mejorar nuestro servicio")
+    if int(X)>5:
+        
+        if int(X)<8:
+            print("Siempre buscamos el mejor servicio para ustedes, ¿nos podría dar un comentatio para mejorar en un futuro?")
+        else:("Puto el que lo lea")
+
